28b827be63c32596e6be69d0467858b3ca3c753c
Leonid-Korobkov <korobkov-leo@mail.ru>
2022-12-14 17:41:06 +0300

    добавлена темная тема, исправлены ошибки

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -126,3 +126,69 @@ function filterElements(list, item, itemDate, property) {
 }
 filterElements('.projects__list', '.projects-item', '.projects-item__date', 'flex')
 filterElements('.project__list', '.project-item', '.project-item__date', 'grid')
+
+// ! Изменение картинок на темной теме
+const imagesDarkMode = document.querySelectorAll('[data-img-src]')
+function addClassDarkForImage() {
+  imagesDarkMode.forEach((img) => {
+    img.src = img.getAttribute('data-img-src')
+  })
+}
+function removeClassDarkForImage() {
+  imagesDarkMode.forEach((img) => {
+    const extensionImg = /\.[^\.]*$/.exec(img.src)[0]
+    img.src = img.src.slice(0, img.src.length - 9) + extensionImg
+  })
+}
+
+//! Смена темы
+const btnDarkMode = document.querySelector('.dark-mode-btn')
+
+// 1. Проверка темной темы на уровне системных настроек
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  btnDarkMode.classList.add('dark-mode-btn--active')
+  document.documentElement.classList.add('dark')
+  addClassDarkForImage()
+}
+
+// 2. Проверка темной темы в localStorage
+if (localStorage.getItem('darkMode') === 'dark') {
+  btnDarkMode.classList.add('dark-mode-btn--active')
+  document.documentElement.classList.add('dark')
+  addClassDarkForImage()
+} else if (localStorage.getItem('darkMode') === 'light') {
+  btnDarkMode.classList.remove('dark-mode-btn--active')
+  document.documentElement.classList.remove('dark')
+  removeClassDarkForImage()
+}
+
+// Если меняются системные настройки, меняем тему
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+  const newColorScheme = event.matches ? 'dark' : 'light'
+
+  if (newColorScheme === 'dark') {
+    btnDarkMode.classList.add('dark-mode-btn--active')
+    document.documentElement.classList.add('dark')
+    localStorage.setItem('darkMode', 'dark')
+    addClassDarkForImage()
+  } else {
+    btnDarkMode.classList.remove('dark-mode-btn--active')
+    document.documentElement.classList.remove('dark')
+    localStorage.setItem('darkMode', 'light')
+    removeClassDarkForImage()
+  }
+})
+
+// Включение ночного режима по кнопке
+btnDarkMode.onclick = function () {
+  btnDarkMode.classList.toggle('dark-mode-btn--active')
+  const isDark = document.documentElement.classList.toggle('dark')
+
+  if (isDark) {
+    addClassDarkForImage()
+    localStorage.setItem('darkMode', 'dark')
+  } else {
+    removeClassDarkForImage()
+    localStorage.setItem('darkMode', 'light')
+  }
+}
